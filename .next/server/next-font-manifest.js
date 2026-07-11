151b766aa223067c7eca8624491f30bd1c66f597
BitnationAI <bitnationai@users.noreply.github.com>
2026-07-11 09:01:20 +0200

    Add .gitignore, remove tracked build files

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/data/data/com.termux/files/home/dr-hina-neuro/src/app/layout":["static/media/e4af272ccee01ff0-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
